c74dfc07821d276e2d7171149570bbf487f86483
Olaf <olaf.chuszno@gmail.com>
2024-09-19 13:17:43 +0200

    Implement basic element editing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { ElementsService } from './elements.service';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { ElementsService } from './elements.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 export interface PeriodicElement {
   position: number;
@@ -12,10 +17,27 @@ export interface PeriodicElement {
   symbol: string;
 }
 
+type ElementField = keyof PeriodicElement;
+
+
+interface ElementUnderEdit {
+  position: number,
+  field: ElementField
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
+  imports: [
+    RouterOutlet,
+    ReactiveFormsModule,
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatDialogModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -31,6 +53,17 @@ export class AppComponent {
 
   filteredElements: PeriodicElement[] = [];
 
+  isPopUpOpen: boolean = false;
+
+  elementUnderEdit: ElementUnderEdit | null = null;
+
+  tableColumns: string[] = [
+    'position',
+    'name',
+    'weight',
+    'symbol'
+  ];
+
   constructor() {
     this.elementsService.getAllElements().then((elements) => {
       this.elements = elements;
@@ -38,11 +71,35 @@ export class AppComponent {
     });
 
     this.filterInput.valueChanges
-      .pipe(
-        debounceTime(2000),
-        distinctUntilChanged()
-      )
+      .pipe(debounceTime(2000), distinctUntilChanged())
       .subscribe((text: string | null) => this.filterElements(text || ''));
+  }
+
+  openPopUp() {
+    this.isPopUpOpen = true;
+  }
+
+  closePopUp() {
+    this.isPopUpOpen = false;
+  }
+
+  startEditingElement(position: number, field: ElementField) {
+    this.isPopUpOpen = !this.isPopUpOpen;
+
+    this.elementUnderEdit = {position, field};
+  }
+
+  finishEditingElement(newValue: string) {
+    const newConvertedValue = this.elementUnderEdit?.field === 'weight' ? +newValue : newValue;
+
+    this.updateElementValue('filteredElements', newConvertedValue);
+
+    this.updateElementValue('elements', newConvertedValue);
+
+    console.log({elements: this.elements})
+
+    this.closePopUp();
+    this.elementUnderEdit = null;
   }
 
   filterElements(text: string): void {
@@ -52,6 +109,19 @@ export class AppComponent {
           .toLocaleLowerCase()
           .includes(text.toLocaleLowerCase())
       );
+    });
+  }
+
+  updateElementValue(elementsArray: 'elements' | 'filteredElements', newConvertedValue: string | number) {
+    this[elementsArray] = this[elementsArray].map((element) => {
+      if (element.position === this.elementUnderEdit?.position) {
+        return {
+          ...element,
+          [this.elementUnderEdit.field]: newConvertedValue
+        }
+      } else {
+        return element;
+      }
     });
   }
 }
