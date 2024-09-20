3f77e018ef8dd8f85aea79a254b326e85064c69a
Olaf <olaf.chuszno@gmail.com>
2024-09-20 10:47:32 +0200

    Extract Dialog component to separate file

---
+++ src/app/ElementChangeDialog/element-change-dialog.component.ts
@@ -0,0 +1,42 @@
+import { Component, Inject } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MAT_DIALOG_DATA, MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { ElementField } from "../App/app.component";
+
+@Component({
+  selector: 'element-change-dialog',
+  templateUrl: 'element-change-dialog.html',
+  styleUrl: './element-change-dialog.scss',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogModule,
+  ],
+})
+
+export class ElementChangeDialog {
+  public data: {
+    position: number
+    elementName: string;
+    field: ElementField
+    currentValue: string | number
+  }
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    data: {
+      position: number;
+      elementName: string;
+      field: ElementField;
+      currentValue: string | number;
+    }
+  ) {
+    this.data = data;
+  }
+}
