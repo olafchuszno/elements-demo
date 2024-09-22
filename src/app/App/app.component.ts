e5d576c67ec93efd7aa65e3f5f656bdac61b0d0d
Olaf <olaf.chuszno@gmail.com>
2024-09-22 19:15:25 +0200

    Implement basic rx-angular state

--- src/app/App/app.component.ts
+++ src/app/App/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener, inject } from '@angular/core';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -11,6 +11,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ElementChangeDialog } from '../ElementChangeDialog/element-change-dialog.component';
 import { LocalStorageService } from '../core/local-storage.service';
+import { rxState } from '@rx-angular/state';
+import { RxPush } from '@rx-angular/template/push';
+import { RxLet } from '@rx-angular/template/let';
+import { RxFor } from '@rx-angular/template/for';
+
 
 export interface PeriodicElement {
   position: number;
@@ -27,6 +32,13 @@ export interface EditedElementData {
   newValue: string | number;
 }
 
+interface State {
+  elements: PeriodicElement[];
+  filteredElements: PeriodicElement[];
+  currentFilter: string;
+  isFetchingElements: boolean;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -39,23 +51,39 @@ export interface EditedElementData {
     MatTableModule,
     MatFormFieldModule,
     MatDialogModule,
+    RxFor,
+    RxLet,
+    RxPush,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'elements-test';
+  private state = rxState<State>(({ set }) => {
+    set({
+      elements: [],
+      filteredElements: [],
+      currentFilter: '',
+      isFetchingElements: false,
+    });
+  });
 
-  isFetchingElements: boolean = false;
+  readonly elements$ = this.state.select('elements')
+  readonly filteredElements$ = this.state.select('filteredElements')
+  readonly currentFilter$ = this.state.select('currentFilter')
+  readonly isFetchingElements$ = this.state.select('isFetchingElements')
+  state$ = this.state.select();
+
 
   elements: PeriodicElement[] = [];
+  filteredElements: PeriodicElement[] = [];
+  isFetchingElements: boolean = false;
+
   elementsService: ElementsService = inject(ElementsService);
 
   localStorageService: LocalStorageService = inject(LocalStorageService);
 
   filterInput = new FormControl('');
-
-  filteredElements: PeriodicElement[] = [];
 
   isPopUpOpen: boolean = false;
 
@@ -65,10 +93,12 @@ export class AppComponent {
 
   tableColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
-  currentFilter: string = '';
-
   constructor() {
-    this.isFetchingElements = true;
+    this.state.connect('isFetchingElements', this.isFetchingElements$);
+    this.state.connect('filteredElements', this.filteredElements$);
+    this.state.connect('elements', this.elements$);
+
+    this.state.set({ isFetchingElements: true });
 
     const localElements = this.localStorageService.getAllElements();
 
@@ -79,17 +109,24 @@ export class AppComponent {
           this.setElements(elements);
         })
         .finally(() => {
-          this.isFetchingElements = false;
+          this.state.set({ isFetchingElements: false });
         });
     } else {
       this.setElements(localElements);
 
-      this.isFetchingElements = false;
+      this.state.set({ isFetchingElements: false });
     }
 
-    this.filterInput.valueChanges
-      .pipe(debounceTime(2000), distinctUntilChanged())
-      .subscribe((text: string | null) => this.filterElements(text || ''));
+    this.state.connect('filteredElements', this.filterInput.valueChanges
+      .pipe(
+        debounceTime(2000),
+        distinctUntilChanged(),
+        map((filterValue) => {
+          this.filterElements(filterValue || '');
+          return this.state.get('filteredElements')
+        })
+      )
+    )
 
     this.updateScreenSize();
   }
@@ -100,8 +137,10 @@ export class AppComponent {
   }
 
   setElements(elements: PeriodicElement[]) {
-    this.elements = elements;
-    this.filteredElements = elements;
+    this.state.set({
+      elements: elements,
+      filteredElements: elements
+    });
   }
 
   openDialog(
@@ -142,29 +181,34 @@ export class AppComponent {
   }
 
   filterElements(text: string): void {
-    this.currentFilter = text;
+    this.state.set({currentFilter: text});
 
-    this.filteredElements = this.elements.filter((element) => {
+    const filteredElements = this.state.get('elements').filter((element) => {
       return Object.values(element).some((elementValue) =>
         String(elementValue)
           .toLocaleLowerCase()
           .includes(text.toLocaleLowerCase())
       );
     });
+
+    this.state.set({filteredElements});
   }
 
+  // TODO: Refactor - just call filterElements with currentFilter from the state
   updateFilteredElements() {
-    this.filteredElements = this.elements.filter((element) => {
+    const filteredElements = this.state.get('elements').filter((element) => {
       return Object.values(element).some((elementValue) =>
         String(elementValue)
           .toLocaleLowerCase()
-          .includes(this.currentFilter.toLocaleLowerCase())
+          .includes(this.state.get('currentFilter').toLocaleLowerCase())
       );
     });
+
+    this.state.set({ filteredElements });
   }
 
   updateElementsInStorage(): void {
-    this.localStorageService.setElements(this.elements);
+    this.localStorageService.setElements(this.state.get('elements'));
   }
 
   /**
@@ -175,7 +219,7 @@ export class AppComponent {
   changeElementValue(editedElementData: EditedElementData) {
     const { position, field, newValue } = editedElementData;
 
-    this.elements = this.elements.map((element) => {
+    const updatedElements = this.state.get('elements').map((element) => {
       if (element.position === position) {
         return {
           ...element,
@@ -185,5 +229,7 @@ export class AppComponent {
         return element;
       }
     });
+
+    this.state.set({elements: updatedElements})
   }
 }
